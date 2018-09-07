9cd493c752d5687c8fa1d29a3fc0a3eeb1dc95dc
Andrés Aguilar <andresyoshimar@gmail.com>
2018-09-07 11:46:44 -0500

    Agregando la estructura del proyecto con JPA y Thymieleaf

---
+++ src/main/java/com/yosh/springboot/jpa/app/SpringBootDataJpa1Application.java
@@ -0,0 +1,12 @@
+package com.yosh.springboot.jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpa1Application.class, args);
+	}
+}
