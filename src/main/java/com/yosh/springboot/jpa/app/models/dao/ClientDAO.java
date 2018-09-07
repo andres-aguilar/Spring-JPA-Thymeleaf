0efe86e02c971bf9edefc938591f2efe1112a683
Andrés Aguilar <andresyoshimar@gmail.com>
2018-09-07 16:35:42 -0500

    Agregando opción para editar los clientes

--- src/main/java/com/yosh/springboot/jpa/app/models/dao/ClientDAO.java
+++ src/main/java/com/yosh/springboot/jpa/app/models/dao/ClientDAO.java
@@ -25,6 +25,15 @@ public class ClientDAO implements IClientDao {
 	@Transactional
 	@Override
 	public void save(Client client) {
-		em.persist(client);		
+		if (client.getId() != null && client.getId() > 0) {
+			em.merge(client);
+		} else {			
+			em.persist(client);		
+		}
+	}
+
+	@Override
+	public Client findOne(Long id) {
+		return em.find(Client.class, id);
 	}
 }
